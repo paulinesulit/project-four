ea3c8b28b1fd1d03505b7ff0ad82ef0903d560c9
Pauline <lynnsulit@gmail.com>
2022-01-25 20:58:19 -0500

    added reading list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <h1>berks</h1>
+      <Link to="/mybooks">My Reading List</Link>
     </header>
   );
 };
--- src/components/UserReadingList.js
+++ src/components/UserReadingList.js
@@ -14,15 +14,50 @@ const [ unreadList, setUnreadList ] = useState([])
 
 useEffect(() => {
   onValue(unreadAddress, (response) => {
-    setUnreadList(response.val())
+    if (response.val() === null) {
+      setUnreadList([]);
+    } else {
+      setUnreadList(Object.entries(response.val()));
+    }
   });
-},[database])
+}, [database]);
 
-  return (
-    <div>
+const handleRemove = (book) => {
+  const database = getDatabase(BooksProject);
 
-    </div>
-  )
+  const dbBookAddress = ref(database, `unreadReadingList/${book}`);
+  remove(dbBookAddress);
+};
+
+return (
+  <div>
+    {unreadList.map((book) => {
+      return (
+        <li key={book[1].id}>
+          <h2>{book[1].title}</h2>
+          {book[1].jacket === undefined ? null : (
+            <img src={book[1].jacket} alt={book[1].title} />
+          )}
+          {book[1].author === undefined ? null : <h3>{book[1].author[0]}</h3>}
+
+          {book[1].pages === undefined ? (
+            <h5>Page count unknown</h5>
+          ) : (
+            <h5>{`${book[1].pages} pages`}</h5>
+          )}
+
+          <button
+            onClick={() => {
+              handleRemove(book[0]);
+            }}
+          >
+            Remove
+          </button>
+        </li>
+      );
+    })}
+  </div>
+);
 }
 
 export default UserReadingList;
