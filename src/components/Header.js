be4b90485003f5ff2add6825d1a9d6323c38f71d
Pauline <lynnsulit@gmail.com>
2022-01-27 17:19:53 -0500

    added some styless

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,27 @@ import ReadingListIcon from "./ReadingListIcon";
 const Header = () => {
   return (
     <header>
-      <nav>
+      <nav className="wrapper">
         <ul>
           <li className="logo">
             <Link to="/" aria-label="Click to go back to the search">
-              <img src="/assets/page-turners-logo.png" alt="Page Turners logo" />
+              <img
+                src="/assets/page-turners-logo.png"
+                alt="Page Turners logo"
+              />
             </Link>
           </li>
           <li>
-            <Link to="/mybooks" aria-label="Click to go to your reading list page">
+            <Link
+              to="/mybooks"
+              aria-label="Click to go to your reading list page"
+            >
               My Reading List
             </Link>
             <ReadingListIcon />
           </li>
         </ul>
       </nav>
-
     </header>
   );
 };
