e8e247a8d8843069ac5c06b9e37bfdb00e5ebfab
Pauline <lynnsulit@gmail.com>
2022-03-25 23:12:01 -0400

    changed README

--- src/firebaseSetup.js
+++ src/firebaseSetup.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,7 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const BooksProject = initializeApp(firebaseConfig);
-
-export const auth = getAuth(BooksProject);
 
 export default BooksProject;
